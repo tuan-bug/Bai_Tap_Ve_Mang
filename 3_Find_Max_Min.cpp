d3f8dfba97e7af3c0a47e0dea4f611822b828202
phuongnam201 <99521352+phuongnam201@users.noreply.github.com>
2022-04-02 16:13:31 +0700

    Update 3_Find_Max_Min.cpp

--- 3_Find_Max_Min.cpp
+++ 3_Find_Max_Min.cpp
@@ -1,22 +1,31 @@
 
-int findmax(int a, int b){
+
+int get_max(int a, int b){
 	if(a < b)
 		return b;
 	return a;
 }
-int findmin(int a, int b){
+
+int get_min(int a, int b){
 	if(a < b)
 		return a;
 	return b;
 }
-void output_array(int a[], int n){
-	int max = a[0];
+
+
+int find_MAX(int arr[], int n){
+	for(int i = 0; i < n; i++){
+		max = findmax(max, arr[i]);
+	}
+	cout<<"phan tu max: "<< max <<endl;
+}
+
+
+void find_MIN(int arr[], int n);{
 	int min = a[0];
 	for(int i = 0; i < n; i++){
-		max = findmax(max, a[i]);
-		min = findmin(min, a[i]);
+		min = findmin(min, arr[i]);
 	}
-	cout<<"phan tu max: "<< max<<endl;
-	cout<<"phan tu min: "<< min;
+	cout<<"phan tu min: "<< min <<endl;
 }
 
